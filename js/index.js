6d168fb55135718e9d9a68f61cc11f568da92b8d
“Adrian” <leal.adrian.vacas@gmail.com>
2023-10-18 20:51:11 +0200

    ArregloImagen

--- js/index.js
+++ js/index.js
@@ -69,7 +69,7 @@ const determinarGanador = (puntosJugadores) => {
         } else {
             alert('Computadora Gana!');
         };
-    }, 2000);
+    }, 1000);
 };
 //----------------------------------
 /**
